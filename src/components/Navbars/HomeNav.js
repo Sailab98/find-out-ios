f0afbc1608101b23e4d0f5b0698816b31ad2d41d
Sailab12345 <sailabbasak1998@gmail.com>
2021-06-20 21:51:00 +0530

    Partial Navbar

---
+++ src/components/Navbars/HomeNav.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Menu, Segment, Dropdown, Icon, Container } from "semantic-ui-react";
+
+const HomeNav = () => {
+  return (
+    <>
+      <Menu size="huge" className="border-raius-none" inverted borderless fixed="top">
+        <Container>
+          <Menu.Item as="a" name="home" header />
+          <Menu.Item
+            as="a"
+            name="messages"
+            // active={activeItem === "messages"}
+            // onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as="a"
+            name="friends"
+            // active={activeItem === "friends"}
+            // onClick={this.handleItemClick}
+          />
+          <Menu.Item>
+            <Dropdown item text="Categories">
+              <Dropdown.Menu>
+                <Dropdown.Item>Electronics</Dropdown.Item>
+                <Dropdown.Item>Automotive</Dropdown.Item>
+                <Dropdown.Item>Home</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <a href="www.wikipedia.com">
+                <Icon.Group>
+                  <Icon size="large" className="heart " circular />
+                  <Icon inverted color="red" corner="top right" name="add" size="huge" />
+                </Icon.Group>
+              </a>
+            </Menu.Item>
+            <Menu.Item>
+              <a href="www.wikipedia.com">
+                <Icon size="large" className="cart "></Icon>
+              </a>
+            </Menu.Item>
+            <Menu.Item name="logout" />
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </>
+  );
+};
+export default HomeNav;
---
+++ src/screens/home-page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeNav from "../components/Navbars/HomeNav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Home = () => {
+  return (
+    <>
+      <HomeNav />
+    </>
+  );
+};
+export default Home;
